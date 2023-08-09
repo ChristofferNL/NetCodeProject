d13bc5edc134ccdaedb9200dfd37d127bc90ed10
ChristofferNL <85027479+ChristofferNL@users.noreply.github.com>
2023-08-09 16:55:33 +0200

    lobby logic

--- Assets/Scripts/TestLobby.cs
+++ Assets/Scripts/TestLobby.cs
@@ -34,7 +34,38 @@ public class TestLobby : MonoBehaviour
         } 
         catch (LobbyServiceException e) 
         { 
-            Debug.Log(e); 
+            Debug.LogError(e); 
+        }
+    }
+
+    public async void ListLobbies()
+    {
+        try
+        {
+            QueryLobbiesOptions queryLobbiesOptions = new QueryLobbiesOptions 
+            {
+                Count = 10,
+                Filters = new List<QueryFilter> 
+                {
+                    new QueryFilter(QueryFilter.FieldOptions.AvailableSlots, "0", QueryFilter.OpOptions.GT)
+                },
+                Order = new List<QueryOrder>
+                {
+                    new QueryOrder(false, QueryOrder.FieldOptions.Created)
+                }
+            };
+
+            QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync();
+            Debug.Log($"Lobbies found: {queryResponse.Results.Count}");
+
+            foreach (var lobby in queryResponse.Results)
+            {
+                Debug.Log($"{lobby.Name} {lobby.MaxPlayers}");
+            }
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.LogError(e);
         }
     }
 }
